8e8d785396c5c249c4a0cbc2e46db2c7cb23b250
braeanneori <58416138+braeanneori@users.noreply.github.com>
2021-02-25 13:53:01 -0500

    lab8 requirements

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = "5d62ASK5zEEyESGIbOJZojXkAK7RxbIf";
+
+  var map = L.mapquest.map('map', {
+    center: [32.8785248, -117.23808],
+    layers: L.mapquest.titleLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.8785248, -117.23808]).addTo(map);
 }
